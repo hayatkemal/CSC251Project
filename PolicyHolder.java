220af2aad75198376cb8a08796514e42990b3b21
hayatkemal <146596721+hayatkemal@users.noreply.github.com>
2023-10-30 19:30:00 -0500

    Update PolicyHolder.java
     "Imported fields and methods from Policy class"

--- PolicyHolder.java
+++ PolicyHolder.java
@@ -1,3 +1,141 @@
 public class PolicyHolder
-{ 
+{
+/*
+      This class is an aggregate class to Policy
+      
+   */
+  private String firstName;
+  private String lastName;
+   private int age;
+   private String smokingStatus;
+   private double height;
+   private double weight;
+
+///No arg constructor for Policy Holder
+
+  public PolicyHolder()
+   {
+      firstName = "";
+      lastName = "";
+      age = 0;
+      smokingStatus = "";
+      height = 0;
+      weight = 0;
+   }
+   //Arg accepting constructor
+
+   public PolicyHolder(String fName, String lName,int a, String sStatus, double h, double w)
+   {
+      firstName = fName;
+      lastName = lName;
+      age = a;
+      smokingStatus = sStatus;
+      height = h;
+      weight = w;
+   }
+   
+   //setters//
+
+   public void setFirstName(String fName)
+   {
+      firstName = fName;
+   }
+   
+   public void setLastName(String lName)
+   {
+      lastName = lName;
+   }
+   
+   public void setAge(int a)
+   {
+      age = a;
+   }
+   
+   public void setSmokingStatus(String sStatus)
+   {
+      smokingStatus = sStatus;
+   }
+   
+   public void setHeight(double h)
+   {
+      height = h;
+   }
+   
+   public void setWeight(double w)
+   {
+      weight = w;
+   }
+
+   
+   //getters//
+   
+  
+   
+   public String getFirstName()
+   {
+      return firstName;
+   }
+   
+   public String getLastName()
+   {
+      return lastName;
+   }
+   
+   public int getAge()
+   {
+      return age;
+   }
+   
+   public String getSmokingStatus()
+   {
+      return smokingStatus;
+   }
+   
+   public double getHeight()
+   {
+      return height;
+   }
+   
+   public double getWeight()
+   {
+      return weight;
+   }
+   
+      
+   //Calculates the Policyholder's BMI
+   public double getBMI()
+   {
+      final double CONVFACTOR = 703;
+      
+      return (weight * CONVFACTOR) / (height * height);
+   }
+   
+   //Calculates the Policy's price
+   public double getPrice()
+   {
+      final double BASE_PRICE = 600;
+      final double ADDITIONAL_FEE_AGE = 75;
+      final double ADDITIONAL_FEE_SMOKING = 100;
+      final double ADDITIONAL_FEE_PER_BMI = 20;
+      
+      final int AGE_THRESHOLD = 50;
+      final int BMI_THRESHOLD = 35;
+      
+      double price = BASE_PRICE;
+      
+      if(age > AGE_THRESHOLD) //over 50 years
+         price += ADDITIONAL_FEE_AGE; //75
+         
+      if(smokingStatus.equalsIgnoreCase("smoker")) 
+         price += ADDITIONAL_FEE_SMOKING; //100
+            
+      //call the getBMI method
+      if(getBMI() > BMI_THRESHOLD) //BMI over 35
+         price += ((getBMI() - BMI_THRESHOLD) * ADDITIONAL_FEE_PER_BMI); //additional BMI fee - 20
+         
+      return price;
+   }
+   
+   
 }
+ 

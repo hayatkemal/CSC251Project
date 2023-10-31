3a6d8673aef0f8727614bf631b506739677a662f
hayatkemal <146596721+hayatkemal@users.noreply.github.com>
2023-10-30 19:42:17 -0500

    Update PolicyHolder.java
    "Added toString method"

--- PolicyHolder.java
+++ PolicyHolder.java
@@ -136,6 +136,21 @@ public class PolicyHolder
       return price;
    }
    
-   
+   //The to Stirng method outputs all info
+   public String toString()
+   {
+             return String.format("\nPolicyholder's First Name: " + firstName +
+             "\nPolicyholder's Last Name: " + lastName +
+             "\nPolicyholder's Age: " + age +
+             "\nPolicyholder's Smoking Status (Y/N): " + smokingStatus +
+             "\nPolicyholder's Height: %,.1f inches\n" +  
+             "Policyholder's Weight: %,.1f pounds\n" + 
+             "Policyholder's BMI: %,.2f\n" +
+             "Policy Price: $%,.2f\n\n", height, weight, getBMI(), getPrice());
+             
+        
+          
+                         
+   }
 }
  
